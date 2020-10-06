a6bde48b2a50e3089bb513b66964b8f912c398e8
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-10-06 22:55:22 +0700

    [add] enableNotification to persist storage

--- app/store/index.ts
+++ app/store/index.ts
@@ -23,7 +23,7 @@ const persistConfig = {
 const systemPersistConfig = {
   key: 'system',
   storage: AsyncStorage,
-  whitelist: ['sessionIsValid'],
+  whitelist: ['sessionIsValid', 'enableNotification'],
 };
 
 const sessionPersistConfig = {
