1fbf4954b9bf85e0b5b4372069605654e777ce38
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-11-19 09:53:59 +0700

    [update] add preview

--- app/utils/environment.ts
+++ app/utils/environment.ts
@@ -1,10 +1,10 @@
 // Production env
-// export const BASE_URL = 'http://3.81.18.101:3000';
-// export const IMAGE_URL = 'http://3.81.18.101:3000';
-// export const SOCKET_URL = 'http://3.81.18.101:8000';
+export const BASE_URL = 'http://3.90.148.26:3000';
+export const IMAGE_URL = 'http://3.90.148.26:3000';
+export const SOCKET_URL = 'http://3.81.18.101:8000';
 
 // Development env
-export const BASE_URL = 'http://192.168.18.36:3000';
-export const IMAGE_URL = 'http://192.168.18.36:3000';
-export const SOCKET_URL = 'http://192.168.18.36:8000';
+// export const BASE_URL = 'http://192.168.18.36:3000';
+// export const IMAGE_URL = 'http://192.168.18.36:3000';
+// export const SOCKET_URL = 'http://192.168.18.36:8000';
 export const RESET_PASSWORD_KEY = 'zwallet1998rst';
